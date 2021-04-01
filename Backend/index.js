1d0374bd476c7552450b6d86f04ea6785d50b73b
Ricardo Prada <prada-r.a@hotmail.com>
2021-04-01 10:25:10 -0500

    setting up Middlewares

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
 
 //Inicializations
 const app = express();
 
 //Settings
 app.set('port', 3000);
+
+//Middlewares
+app.use(morgan('dev'));
+// **Multer intrerpreta las img que se suban al server
+const storage = multer.diskStorage({
+  destination: path.join(__dirname,'public/uploads'),
+  filename(req, file, cb) {
+    cb(null, new Date.getTime() + path.extname(file.originalname))
+  }
+});
+app.use(multer({storage}).single('image'));
+// **interpretar datos de formularios como JSON
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 //Start the server
 app.listen(app.get('port'), () => {

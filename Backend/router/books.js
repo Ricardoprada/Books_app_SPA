c062192900ce0d62fa070b62152718c68d09bd50
Ricardo Prada <prada-r.a@hotmail.com>
2021-04-04 20:48:43 -0500

    setting api rest

--- Backend/router/books.js
+++ Backend/router/books.js
@@ -1,6 +1,25 @@
 const {Router} = require('express');
 const router = Router();
 
-router.get('/', (req, res) => res.json({text: "Hello World"}));
+const Book = require('../models/Books');
+
+router.get('/', async (req, res) => {
+  //**find will search all books
+  const books = await Book.find();
+  res.json(books);
+});
+
+router.post('/', async (req, res) => {
+  const {title, author, isbn} = req.body;
+  const newBook = new Book({title, author, isbn});
+  console.log(newBook)
+  await newBook.save();
+  res.json({message: "Book Saved"});
+});
+
+router.delete('/:id', async (req, res) => {
+  const book = await Book.findByIdAndDelete(req.params.id);
+  res.json({message: "Book Deleted"});
+});
 
 module.exports = router;

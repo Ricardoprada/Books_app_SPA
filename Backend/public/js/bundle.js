85ea23688d1fda4ea61bbf9f5bc895c0ddca7859
Ricardo Prada <prada-r.a@hotmail.com>
2021-04-05 20:11:22 -0500

    configuring dev dependencies - webpack

---
+++ Backend/public/js/bundle.js
@@ -0,0 +1,2 @@
+(()=>{var e={100:(e,o,r)=>{"use strict";r.r(o)}},o={};function r(t){var l=o[t];if(void 0!==l)return l.exports;var n=o[t]={exports:{}};return e[t](n,n.exports,r),n.exports}r.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},console.log("Hello World"),r(100)})();
+//# sourceMappingURL=bundle.js.map
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devMode = process.env.NODE_ENV !== "production";
+
+module.exports = {
+
+  entry: './FrontEnd/app.js',
+  output: {
+    path: path.join(__dirname, 'BackEnd/public'),
+    filename: 'js/bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          devMode ? 'style-loader': MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './FrontEnd/index.html',
+
+      //minify code  
+      minify: {
+        //remove blanks
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+
+      },
+
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/bundle.css"
+    })
+  ],
+
+  devtool: 'source-map'
+
+}
